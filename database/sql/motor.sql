501d7c02380d29df1f17d53d0c8582efc8fbaab4
Panagiotis K <pkoutsogian@hotmail.com>
2019-06-27 20:56:12 +0300

    Added send to db

--- database/sql/motor.sql
+++ database/sql/motor.sql
@@ -3,11 +3,12 @@
 -- ---------------------------
 CREATE TABLE IF NOT EXISTS `motor` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `Temperature` DECIMAL(6,2) DEFAULT '0',
-  `VoltageIn` DECIMAL(6,2) DEFAULT '0',
-  `RPM` INT DEFAULT '0',
-  `PowerIn` DECIMAL(8,2) DEFAULT '0',
-  `CurrentIn` DECIMAL(4,2) DEFAULT '0',
+  `speed` INT DEFAULT '0',
+  `throttle` INT DEFAULT '0',
+  `current` DECIMAL(4,2) DEFAULT '0',
+  `voltage` DECIMAL(6,2) DEFAULT '0',
+  `contrTemp` DECIMAL(6,2) DEFAULT '0',
+  `motorTemp` DECIMAL(6,2) DEFAULT '0',
   `Time` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `serverSideName` VARCHAR(15) DEFAULT 'motor',
   PRIMARY KEY (`id`)
